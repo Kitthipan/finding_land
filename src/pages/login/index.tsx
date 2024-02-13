a2932e56e57ebdcfadf30370f74a8204c6c3fa7c
296418 <kitthipan.tho@bigc.co.th>
2024-02-13 16:20:51 +0700

    login font

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Image from 'next/image'
+import Logo from '/public/catLogo.png'
+import { useForm } from 'react-hook-form'
+
+type Props = {}
+
+type LoginState = {
+    username: string
+    password: string
+  }
+
+const LoginPage = (props: Props) => {
+    const { register, handleSubmit, watch } = useForm<LoginState>()
+
+    
+
+    const handleLogin =(data:LoginState)=>{
+        console.log("data: ",data)
+
+    }
+
+    return (
+        <div>
+            <div className='flex  items-center justify-center  min-h-screen'>
+                <div className='bg-white p-6 rounded-lg shadow-lg w-full sm:w-96'>
+                    <div className='flex justify-center'>
+                        <Image
+                            src={Logo}
+                            alt='Logo'
+                            width='90'
+                            height='90'
+                            className='cursor-pointer mb-4'
+                            priority
+                        />
+                    </div>
+                    <form onSubmit={handleSubmit(handleLogin)}>
+                        <div className='flex gap-4 p-1 mb-4'>
+                            <label htmlFor='username' className='block text-gray-700'>
+                                Username
+                            </label>
+                            <input
+                                type='text'
+                                id='username'
+                                className='w-full border rounded-md p-2'
+                                autoComplete='off'
+                                {...register('username')}
+                            />
+                        </div>
+                        <div className='flex gap-4  p-1 mb-4'>
+                            <label htmlFor='password' className='block text-gray-700'>
+                                password
+                            </label>
+                            <input
+                                type='password'
+                                id='password'
+                                className='w-full border rounded-md p-2'
+                                autoComplete='off'
+                                {...register('password')}
+                            />
+                        </div>
+                        <button type='submit' className='border w-full rounded bg-gray-600' >login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
